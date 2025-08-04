82f266c7a3b27424f45cf3711c27d2a50790ae69
Dhanushka <dhanushkamcr@gmail.com>
2025-08-04 10:17:13 -0500

    Applied feedback to the form

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -24,6 +24,7 @@ const UserForm = () => {
     const [user, setUser] = useState(null);
     const [error, setError] = useState(null);
     const [showModal, setShowModal] = useState(false);
+    const [validated, setValidated] = useState(false);
 
     const handleCloseModal = () => setShowModal(false);
 
@@ -38,17 +39,24 @@ const UserForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        try {
-            const response = await axios.post('https://jsonplaceholder.typicode.com/users', formData);
-            console.log(response.data);
-            setUser(response.data);
-            setSubmitted(true);
-            setShowModal(true);
-            setError(null);
-        } catch (err) {
-            setError(`Error submitting the form. Please try again: ${err.message}`);
-            setSubmitted(false);
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.stopPropagation();
+        } else {
+
+            try {
+                const response = await axios.post('https://jsonplaceholder.typicode.com/users', formData);
+                console.log(response.data);
+                setUser(response.data);
+                setSubmitted(true);
+                setShowModal(true);
+                setError(null);
+            } catch (err) {
+                setError(`Error submitting the form. Please try again: ${err.message}`);
+                setSubmitted(false);
+            }
         }
+        setValidated(true);
     };
 
     return (
@@ -60,7 +68,7 @@ const UserForm = () => {
             {submitted && <Alert variant="success" dismissible>{user.name} created successfully!</Alert>}
             {error && <Alert variant="danger" dismissible>{error}</Alert>}
 
-            <Form onSubmit={handleSubmit}>
+            <Form onSubmit={handleSubmit} noValidate validated={validated}>
             <Row>
                 <Col md="5">
                 <Form.Group controlId="formName" className="mb-3">
@@ -73,6 +81,10 @@ const UserForm = () => {
                     onChange={handleChange}
                     required
                     />
+
+                    <Form.Control.Feedback type="invalid">
+                        Please provide a name
+                    </Form.Control.Feedback>
                 </Form.Group>
                 </Col>
 
@@ -87,70 +99,87 @@ const UserForm = () => {
                     onChange={handleChange}
                     required
                     />
+
+                    <Form.Control.Feedback type="invalid">
+                        Please provide an email
+                    </Form.Control.Feedback>
                 </InputGroup>
                 </Col>
             </Row>
 
             <Row>
                 <Col md="7">
-                <FloatingLabel controlId="floatingPhone" label="Phone" className="mb-3" style={{ marginTop: '12px' }}>
-                    <Form.Control
-                    type="number"
-                    placeholder="Phone"
-                    name="phone"
-                    value={formData.phone}
-                    onChange={handleChange}
-                    required
-                    />
-                </FloatingLabel>
+                    <FloatingLabel controlId="floatingPhone" label="Phone" className="mb-3" style={{ marginTop: '12px' }}>
+                        <Form.Control
+                        type="number"
+                        placeholder="Phone"
+                        name="phone"
+                        value={formData.phone}
+                        onChange={handleChange}
+                        required
+                        />
+
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a phone number
+                        </Form.Control.Feedback>
+                    </FloatingLabel>
                 </Col>
 
                 <Col md="5">
-                <Form.Group controlId="formSelect" className="mb-3">
-                    <Form.Label>Select Favorite Food</Form.Label>
-                    <Form.Select
-                    name="food"
-                    value={formData.food}
-                    onChange={handleChange}
-                    required
-                    >
-                    <option hidden value="">Choose...</option>
-                    <option>Pizza</option>
-                    <option>Steak</option>
-                    <option>Ice Cream</option>
-                    <option>Other</option>
-                    </Form.Select>
-                </Form.Group>
+                    <Form.Group controlId="formSelect" className="mb-3">
+                        <Form.Label>Select Favorite Food</Form.Label>
+                        <Form.Select
+                        name="food"
+                        value={formData.food}
+                        onChange={handleChange}
+                        required
+                        >
+                            <option hidden value="">Choose...</option>
+                            <option>Pizza</option>
+                            <option>Steak</option>
+                            <option>Ice Cream</option>
+                            <option>Other</option>
+                        </Form.Select>
+
+                        <Form.Control.Feedback type="invalid">
+                            Please select a food
+                        </Form.Control.Feedback>
+                    </Form.Group>
                 </Col>
             </Row>
 
             <Form.Group className="mb-3" controlId="formRadio">
                 <Form.Label>Preferred method of communication:</Form.Label>
-                <Form.Check
-                type="radio"
-                id="email"
-                name="communication"
-                label="Email"
-                value="Email"
-                onChange={handleChange}
-                required
-                />
-                <Form.Check
-                type="radio"
-                id="phone"
-                name="communication"
-                label="Phone"
-                value="Phone"
-                onChange={handleChange}
-                required
-                />
-            </Form.Group>
+                    <Form.Check
+                        type="radio"
+                        id="email"
+                        name="communication"
+                        label="Email"
+                        value="Email"
+                        onChange={handleChange}
+                        required
+                    />
+                    <Form.Check
+                        type="radio"
+                        id="phone"
+                        name="communication"
+                        label="Phone"
+                        value="Phone"
+                        onChange={handleChange}
+                        required
+                    />
 
-            <Button variant="primary" type="submit" className="mt-3">
-                Submit
-            </Button>
+                    {validated && !formData.communication && (
+                        <Form.Control.Feedback type="invalid" className="d-block">
+                            Please select a communication method
+                        </Form.Control.Feedback>
+                    )}
+                </Form.Group>
+
+                <Button variant="primary" type="submit" className="mt-3">
+                    Submit
+                </Button>
             </Form>
-
         </Container>
     );
 };
